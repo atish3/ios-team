08ae2e8899a220d26f4f4e39e2d027dabbdb3606
Pascal Sturmfels <psturm@umich.edu>
2016-02-01 19:28:53 -0500

    started update on textField height expand

--- MCConnect/MCConnect/MCChatNavigationController.swift
+++ MCConnect/MCConnect/MCChatNavigationController.swift
@@ -12,7 +12,7 @@ class MCChatNavigationController: UIViewController, UITextViewDelegate {
     
     var accessoryToolbar: UIToolbar = UIToolbar()
     var sendButton: UIBarButtonItem = UIBarButtonItem()
-    var MCtextField: UITextView = UITextView()
+    var MCtextView: UITextView = UITextView()
     
     var tableView: MCChatTableViewController!
     
@@ -24,49 +24,62 @@ class MCChatNavigationController: UIViewController, UITextViewDelegate {
         self.title = "MC Chat App"
         
         sendButton = UIBarButtonItem(title: "Send", style: UIBarButtonItemStyle.Plain, target: self, action: "returnTextField")
-        MCtextField = UITextView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width / 2, height: 30))
-        MCtextField.layer.borderWidth = 0.3
-        MCtextField.layer.borderColor = UIColor.grayColor().CGColor
-        MCtextField.delegate = self
-        MCtextField.layer.cornerRadius = 7.0
-        MCtextField.font = UIFont(name: "Helvetica", size: 14.0)!
+        MCtextView = UITextView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width / 2, height: 30))
+        MCtextView.layer.borderWidth = 0.3
+        MCtextView.layer.borderColor = UIColor.grayColor().CGColor
+        MCtextView.delegate = self
+        MCtextView.layer.cornerRadius = 7.0
+        MCtextView.font = UIFont(name: "Helvetica", size: 14.0)!
+        MCtextView.text = "X"
+        MCtextView.frame.size.height = MCtextView.contentSize.height
+        MCtextView.text = ""
+
+        accessoryToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width * 5 / 6, height: 44))
         
-        accessoryToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.width * 3 / 4, height: 44))
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: self, action: nil)
         let fixedSpace = UIBarButtonItem(barButtonSystemItem: .FixedSpace, target: self, action: nil)
         fixedSpace.width = 20
         
-        accessoryToolbar.items = [flexibleSpace, UIBarButtonItem(customView: MCtextField), fixedSpace, sendButton, flexibleSpace]
-        MCtextField.becomeFirstResponder()
+        accessoryToolbar.items = [flexibleSpace, UIBarButtonItem(customView: MCtextView), fixedSpace, sendButton, flexibleSpace]
         
+        MCtextView.becomeFirstResponder()
     }
     
     func returnTextField()
     {
-        if let text = MCtextField.text where text.characters.count > 0 {
+        if let text = MCtextView.text where text.characters.count > 0 {
             UIView.animateWithDuration(0.3, delay: 0, options: [], animations: { () -> Void in
                 self.sendButton.enabled = false
-                self.MCtextField.backgroundColor = UIColor.greenColor()
-                self.MCtextField.textColor = UIColor.whiteColor()
-                self.MCtextField.layer.cornerRadius = 10.0
-                self.MCtextField.layer.borderWidth = 0.0
+                self.MCtextView.backgroundColor = UIColor(red: 0.04, green: 0.93, blue: 0.094, alpha: 1.0)
+                self.MCtextView.textColor = UIColor.whiteColor()
+                self.MCtextView.layer.cornerRadius = 10.0
+                self.MCtextView.layer.borderWidth = 0.0
                 //self.sendButton.tintColor = UIColor.clearColor()
                 }, completion: { (_) -> Void in
                 UIView.animateWithDuration(0.3, delay: 0, options: [], animations: { () -> Void in
-                    self.MCtextField.center.x += self.view.bounds.width
+                    self.MCtextView.center.x += self.view.bounds.width
                     }, completion: { (_) -> Void in
                     //self.sendButton.tintColor = nil
                     self.sendButton.enabled = true
                     self.tableView.addMessage(text, date: NSDate(), type: MCChatMessageType.sentMessage)
-                    self.MCtextField.text = ""
-                    self.MCtextField.textColor = UIColor.blackColor()
-                    self.MCtextField.backgroundColor = UIColor.whiteColor()
-                    self.MCtextField.layer.cornerRadius = 7.0
-                    self.MCtextField.layer.borderWidth = 0.3
-                    self.MCtextField.center.x -= self.view.bounds.width
+                    self.MCtextView.text = ""
+                    self.MCtextView.frame.size.height = self.MCtextView.contentSize.height
+                    self.MCtextView.textColor = UIColor.blackColor()
+                    self.MCtextView.backgroundColor = UIColor.whiteColor()
+                    self.MCtextView.layer.cornerRadius = 7.0
+                    self.MCtextView.layer.borderWidth = 0.3
+                    self.MCtextView.center.x -= self.view.bounds.width
                 })
             })
-            //MCtextField.resignFirstResponder()
+            //MCtextView.resignFirstResponder()
+        }
+    }
+    
+    func textViewDidChange(textView: UITextView) {
+        let numLines = textView.contentSize.height / textView.font!.lineHeight
+        if numLines <= 5
+        {
+            textView.frame.size.height = textView.contentSize.height
         }
     }
     
@@ -85,7 +98,7 @@ class MCChatNavigationController: UIViewController, UITextViewDelegate {
     
     func dismissKeyboard()
     {
-        MCtextField.resignFirstResponder()
+        MCtextView.resignFirstResponder()
     }
     
     override func canBecomeFirstResponder() -> Bool {
