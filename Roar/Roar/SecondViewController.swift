ad518695e15620062dcae5c0c8b0bf9e2ad59b6b
Pascal Sturmfels <psturm@umich.edu>
2016-04-12 16:38:11 -0400

    started profile

--- Roar/Roar/SecondViewController.swift
+++ Roar/Roar/SecondViewController.swift
@@ -9,29 +9,15 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-    var clickMeButton: UIButton!
-
-    @IBOutlet weak var textLabel: UILabel!
+    var profileImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        clickMeButton = UIButton(type: .System)
-        clickMeButton.setTitle("Click Me!", forState: UIControlState.Normal)
-        clickMeButton.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
-        clickMeButton.sizeToFit()
-        clickMeButton.addTarget(self, action: #selector(SecondViewController.changeText), forControlEvents: .TouchUpInside)
-        clickMeButton.translatesAutoresizingMaskIntoConstraints = false
+        profileImage = UIImageView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+        profileImage.image = UIImage(named: "profile_default")
         
-        self.view.addSubview(clickMeButton)
-        let centerXConstraint = NSLayoutConstraint(item: self.view, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: clickMeButton, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)
-        let centerYConstraint = NSLayoutConstraint(item: self.view, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: clickMeButton, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: 100)
-        
-        self.view.addConstraints([centerXConstraint, centerYConstraint])
-    }
-
-    func changeText() {
-        textLabel.text = String(arc4random_uniform(1000))
+        self.view.addSubview(profileImage)
     }
 
     override func didReceiveMemoryWarning() {
