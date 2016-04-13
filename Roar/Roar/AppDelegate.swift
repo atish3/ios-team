95a0c0135ea2b895fd96e9e75f4dbfd526e9e465
Pascal Sturmfels <psturm@umich.edu>
2016-04-13 15:15:46 -0400

    Added core data light migration support

--- Roar/Roar/AppDelegate.swift
+++ Roar/Roar/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        self.saveContext()
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -42,9 +43,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         self.saveContext()
     }
-
-    
-    
     
     // MARK: - Core Data stack
     
@@ -61,28 +59,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
     
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
+        // Initialize Persistent Store Coordinator
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        // URL Documents Directory
+        let URLs = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let applicationDocumentsDirectory = URLs[(URLs.count - 1)]
+        
+        // URL Persistent Store
+        let URLPersistentStore = self.applicationStoresDirectory().URLByAppendingPathComponent("Roar.sqlite")
+    
         do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            // Add Persistent Store to Persistent Store Coordinator
+            let options = [ NSMigratePersistentStoresAutomaticallyOption : true , NSInferMappingModelAutomaticallyOption : true ]
             
-            dict[NSUnderlyingErrorKey] = error as NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-            abort()
+            try persistentStoreCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: URLPersistentStore, options: options)
+            
+        } catch {
+            let fm = NSFileManager.defaultManager()
+            if fm.fileExistsAtPath(URLPersistentStore.path!) {
+                let nameIncompatibleStore = self.nameForIncompatibleStore()
+                let URLCorruptPersistentStore = self.applicationIncompatibleStoresDirectory().URLByAppendingPathComponent(nameIncompatibleStore)
+                
+                do {
+                    // Move Incompatible Store
+                    try fm.moveItemAtURL(URLPersistentStore, toURL: URLCorruptPersistentStore)
+                    
+                    do {
+                        // Declare Options
+                        let options = [ NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true ]
+                        
+                        // Add Persistent Store to Persistent Store Coordinator
+                        try persistentStoreCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: URLPersistentStore, options: options)
+                        
+                    } catch {
+                        let storeError = error as NSError
+                        print("\(storeError), \(storeError.userInfo)")                        
+                        // Update User Defaults
+                        let userDefaults = NSUserDefaults.standardUserDefaults()
+                        userDefaults.setBool(true, forKey: "didDetectIncompatibleStore")
+                    }
+                    
+                } catch {
+                    let moveError = error as NSError
+                    print("\(moveError), \(moveError.userInfo)")
+                }
+                
+            }
         }
         
-        return coordinator
+        return persistentStoreCoordinator
     }()
     
     lazy var managedObjectContext: NSManagedObjectContext = {
@@ -94,6 +120,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
     
     // MARK: - Core Data Saving support
+    
+     private func applicationStoresDirectory() -> NSURL {
+        let fm = NSFileManager.defaultManager()
+        
+        // Fetch Application Support Directory
+        let URLs = fm.URLsForDirectory(.ApplicationSupportDirectory, inDomains: .UserDomainMask)
+        let applicationSupportDirectory = URLs[(URLs.count - 1)]
+        
+        // Create Application Stores Directory
+        let URL = applicationSupportDirectory.URLByAppendingPathComponent("Stores")
+        
+        if !fm.fileExistsAtPath(URL.path!) {
+            do {
+                // Create Directory for Stores
+                try fm.createDirectoryAtURL(URL, withIntermediateDirectories: true, attributes: nil)
+                
+            } catch {
+                let createError = error as NSError
+                print("\(createError), \(createError.userInfo)")
+            }
+        }
+        
+        return URL
+    }
+    
+    private func nameForIncompatibleStore() -> String {
+        // Initialize Date Formatter
+        let dateFormatter = NSDateFormatter()
+        
+        // Configure Date Formatter
+        dateFormatter.formatterBehavior = .Behavior10_4
+        dateFormatter.dateFormat = "yyyy-MM-dd-HH-mm-ss"
+        
+        return "\(dateFormatter.stringFromDate(NSDate())).sqlite"
+    }
+    
+    private func applicationIncompatibleStoresDirectory() -> NSURL {
+        let fm = NSFileManager.defaultManager()
+        
+        // Create Application Incompatible Stores Directory
+        let URL = applicationStoresDirectory().URLByAppendingPathComponent("Incompatible")
+        
+        if !fm.fileExistsAtPath(URL.path!) {
+            do {
+                // Create Directory for Stores
+                try fm.createDirectoryAtURL(URL, withIntermediateDirectories: true, attributes: nil)
+                
+            } catch {
+                let createError = error as NSError
+                print("\(createError), \(createError.userInfo)")
+            }
+        }
+        return URL
+    }
     
     func saveContext () {
         if managedObjectContext.hasChanges {
